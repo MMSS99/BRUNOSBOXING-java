ec242e1b2de99ad6d01145cb10732cc10d184a61
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-10 14:25:32 +0100

    feat(Round): :sparkles: add PointsDeducted & tests

--- src/main/java/edu/estatuas/domain/round/PointsDeducted.java
+++ src/main/java/edu/estatuas/domain/round/PointsDeducted.java
@@ -1,4 +1,43 @@
 package edu.estatuas.domain.round;
 
-public class PointsDeducted {
+public class PointsDeducted implements Round {
+
+    private byte redBoxerScore;
+    private byte blueBoxerScore;
+    private final String roundScore;
+
+    PointsDeducted(String scoreInput) {
+        this.roundScore = scoreInput;
+        boxerRoundScore();
+    }
+
+    @Override
+    public void boxerRoundScore(){
+
+        String removingComma = this.roundScore;
+        while (removingComma.contains(",")) {
+            if (removingComma.indexOf(",") > removingComma.length()/2) {
+                removingComma = removingComma.substring(0, removingComma.indexOf(","));
+            } else {
+                removingComma = removingComma.substring(removingComma.indexOf(",")+2, removingComma.length());
+            }
+        }
+
+        String[] splittedScore = removingComma.split(" - ");
+
+        this.redBoxerScore = Byte.parseByte(splittedScore[0]);
+        this.blueBoxerScore = Byte.parseByte(splittedScore[1]);
+
+    }
+
+    @Override
+    public byte getRedBoxerScore(){
+        return this.redBoxerScore;
+    }
+
+    @Override
+    public byte getBlueBoxerScore(){
+        return this.blueBoxerScore;
+    }
+
 }
--- src/test/java/edu/estatuas/domain/round/RoundTest.java
+++ src/test/java/edu/estatuas/domain/round/RoundTest.java
@@ -25,10 +25,30 @@ class RoundTest {
     }
 
     @Test
+    public void pointsDeductedTest(){
+        PointsDeducted testround = new PointsDeducted("1, 8 - 10");
+
+        assertEquals(8, testround.getRedBoxerScore());
+        assertEquals(10, testround.getBlueBoxerScore());
+
+        PointsDeducted testround2 = new PointsDeducted("10 - 8, 1");
+
+        assertEquals(10, testround2.getRedBoxerScore());
+        assertEquals(8, testround2.getBlueBoxerScore());
+
+        PointsDeducted testround3 = new PointsDeducted("1, 9 - 8, 1");
+
+        assertEquals(9, testround3.getRedBoxerScore());
+        assertEquals(8, testround3.getBlueBoxerScore());
+
+
+    }
+
+    @Test
     public void roundTest(){
         Round testround = new RegularRound("9 - 10");
 
-        assertEquals(9, testround.redBoxerScore);
-        assertEquals(10, testround.blueBoxerScore);
+        assertEquals(9, testround.getRedBoxerScore());
+        assertEquals(10, testround.getBlueBoxerScore());
     }
 }

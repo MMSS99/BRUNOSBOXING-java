dc637a540781d3d66c5413e87d3dc4f8dd6708ec
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-10 15:08:29 +0100

    feat(Scorecard): :sparkles: add Scorecard simple setters, loaders and Round array builder & tests

--- src/main/java/edu/estatuas/domain/scorecard/Scorecard.java
+++ src/main/java/edu/estatuas/domain/scorecard/Scorecard.java
@@ -1,4 +1,44 @@
 package edu.estatuas.domain.scorecard;
 
+import edu.estatuas.domain.round.KnockdownRound;
+import edu.estatuas.domain.round.PointsDeducted;
+import edu.estatuas.domain.round.RegularRound;
+import edu.estatuas.domain.round.Round;
+
 public class Scorecard {
+    String color;
+    String redCorner;
+    String blueCorner;
+    String[] judgeScoreCard;
+    Round[] rounds = new Round[10];
+
+    Scorecard(String color){
+        this.color = color;
+    }
+
+    public void setRedCorner(String boxername){
+        this.redCorner = boxername;
+    }
+
+    public void setBlueCorner(String boxername){
+        this.blueCorner = boxername;
+    }
+
+    public void loadJudgeScoreCard(String[] rawScorecard){
+        this.judgeScoreCard = rawScorecard;
+        loadRounds();
+    }
+
+    private void loadRounds(){
+        for (int roundNumber = 0; roundNumber < rounds.length; roundNumber++){
+            if (judgeScoreCard[roundNumber].contains(",")){
+                rounds[roundNumber] = new PointsDeducted(judgeScoreCard[roundNumber]);
+            } else if (judgeScoreCard[roundNumber].contains("10") && judgeScoreCard[roundNumber].contains("9")) {
+                rounds[roundNumber] = new RegularRound(judgeScoreCard[roundNumber]);
+            } else {
+                rounds[roundNumber] = new KnockdownRound(judgeScoreCard[roundNumber]);
+            }
+        }
+    }
+
 }
